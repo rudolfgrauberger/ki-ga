c341d3a78b0cb02ea57f6791c352a5ac1695cd58
Rudolf Grauberger <rudolf@grauberger.org>
2018-10-24 11:45:40 +0200

    Unified indent spaces

--- Assets/Scripts/GA/Initializers/RoundRobinInitializer.cs
+++ Assets/Scripts/GA/Initializers/RoundRobinInitializer.cs
@@ -4,45 +4,45 @@ using System.Collections.Generic;
 
 public class RoundRobinInitializer : IInitializer
 {
-   private List<char> genes;
-   private Random rand;
+    private List<char> genes;
+    private Random rand;
 
-   public void AssignGene(char ID)
-   {
-      if (genes == null)
-      {
-         genes = new List<char>();
-      }
+    public void AssignGene(char ID)
+    {
+          if (genes == null)
+          {
+                genes = new List<char>();
+          }
 
-      genes.Add(ID);
-   }
+          genes.Add(ID);
+    }
 
-   public List<Individual> CreateInitialGeneration(int generationSize, int individualSize)
-   {
-      List<Individual> list = new List<Individual>();
-      for (int i = 0; i < generationSize; i++)
-      {
-         list.Add(GenerateIndividual(individualSize));
-      }
-      return list;
-   }
+    public List<Individual> CreateInitialGeneration(int generationSize, int individualSize)
+    {
+        List<Individual> list = new List<Individual>();
+        for (int i = 0; i < generationSize; i++)
+        {
+            list.Add(GenerateIndividual(individualSize));
+        }
+        return list;
+    }
 
-   private Individual GenerateIndividual(int individualSize)
-   {
-      if (rand == null)
-      {
+    private Individual GenerateIndividual(int individualSize)
+    {
+        if (rand == null)
+        {
          rand = new Random();
-      }
+        }
 
-      int max = genes.Count;
-      int pos = rand.Next(0, max);
-      Individual ind = new Individual();
-      System.Text.StringBuilder builder = new System.Text.StringBuilder(individualSize);
-      for (int i = 0; i < individualSize; i++)
-      {
+        int max = genes.Count;
+        int pos = rand.Next(0, max);
+        Individual ind = new Individual();
+        System.Text.StringBuilder builder = new System.Text.StringBuilder(individualSize);
+        for (int i = 0; i < individualSize; i++)
+        {
          builder.Append(genes[pos++ % max]);
-      }
-      ind.GeneSequence = builder.ToString();
-      return ind;
-   }
+        }
+        ind.GeneSequence = builder.ToString();
+        return ind;
+    }
 }
