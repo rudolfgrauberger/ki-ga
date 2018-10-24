f4a7344650aba3cc93afbab2058acec236bbc96b
Rudolf Grauberger <rudolf@grauberger.org>
2018-10-24 11:45:12 +0200

    Adds StartAndEndOptimizedInitializer #2

---
+++ Assets/Scripts/GA/Initializers/StartAndEndOptimizedInitializer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StartAndEndOptimizedInitializer : IInitializer
+{
+    private List<char> genes;
+    private Random rand;
+
+    public void AssignGene(char ID)
+    {
+        if (genes == null)
+        {
+            genes = new List<char>();
+        }
+
+        genes.Add(ID);
+    }
+
+    public List<Individual> CreateInitialGeneration(int generationSize, int individualSize)
+    {
+        List<Individual> list = new List<Individual>();
+        for (int i = 0; i < generationSize; i++)
+        {
+            list.Add(GenerateIndividual(individualSize));
+        }
+        return list;
+    }
+
+    private Individual GenerateIndividual(int individualSize)
+    {
+        if (rand == null)
+        {
+            rand = new Random();
+        }
+
+        int max = genes.Count;
+        int pos = rand.Next(0, max);
+        Individual ind = new Individual();
+        System.Text.StringBuilder builder = new System.Text.StringBuilder(individualSize);
+        for (int i = 0; i < individualSize; i++)
+        {
+            builder.Append(genes[pos++ % max]);
+        }
+
+        builder[0] = 'B';
+        builder[builder.Length -1] = 'E';
+        
+        ind.GeneSequence = builder.ToString();
+        return ind;
+   }
+}
